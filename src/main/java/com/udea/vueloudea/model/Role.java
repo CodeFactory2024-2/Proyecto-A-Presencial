a36b6ac0347c7b95c6696ea452d108d551c09966
W10Go <diegoa.carvajal@udea.edu.co>
2024-10-15 06:20:52 -0500

    Segundo intento... AHHH

--- src/main/java/com/udea/vueloudea/model/Role.java
+++ src/main/java/com/udea/vueloudea/model/Role.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Role implements Serializable {
+public class Role{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
